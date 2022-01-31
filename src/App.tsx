e9941abf926f23056177de0fc1b2e1c37f298979
Thiago Silva <thiagof89.tf@gmail.com>
2022-01-31 12:15:34 -0300

    desafio finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,16 @@ import Header from './components/Header';
 import { CartProvider } from './hooks/useCart';
 
 const App = (): JSX.Element => {
-  return (
-    <BrowserRouter>
-      <CartProvider>
-        <GlobalStyles />
-        <Header />
-        <Routes />
-        <ToastContainer autoClose={3000} />
-      </CartProvider>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<CartProvider>
+				<GlobalStyles />
+				<Header />
+				<Routes />
+				<ToastContainer autoClose={3000} />
+			</CartProvider>
+		</BrowserRouter>
+	);
 };
 
 export default App;
